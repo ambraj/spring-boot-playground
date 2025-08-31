1e555bf482d221b692c28053aaf729ea6046ee36
Ambuj <ambraj@gmail.com>
2025-08-31 11:34:31 -0500

    initial

---
+++ src/main/java/com/pm/datajpamapping/DataJpaMappingApplication.java
@@ -0,0 +1,13 @@
+package com.pm.datajpamapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataJpaMappingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataJpaMappingApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/pm/datajpamapping/entity/Department.java
@@ -0,0 +1,36 @@
+package com.pm.datajpamapping.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @OneToOne
+    private Doctor headDoctor;
+
+    @ManyToMany
+    private Set<Doctor> doctors = new HashSet<>();
+
+}
